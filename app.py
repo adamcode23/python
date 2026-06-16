9f8a6df2daf9eeb5539759425ab50fb665e581cf
adamcode23 <adamrz2011@gmail.com>
2026-06-16 18:12:28 +0200

    slt

---
+++ app.py
@@ -0,0 +1,6 @@
+mot_de_passe = (input("quel est le mot de passe ? "))
+
+if mot_de_passe == "1234":
+    print("mot de passe correct !")
+else: 
+    print("mot de passe incorrect !")==
--- meteo.py
+++ meteo.py
@@ -5,4 +5,4 @@ if celciucs < 15:
     print("waa i fait super froid")
 farenheit = celciucs * 9 / 5 + 32
 kelvin = celciucs + 273.15
-print(("la température en farenheit est de ") + str(farenheit) + (" °F !") + (" et en kelvin de ") + str(kelvin) + (" K ! (paye ton café stp le gooooooaaaaaat!!)"))
+print(("la température en farenheit est de ") + str(farenheit) + (" °F !") + (" et en kelvin de ") + str(kelvin) + (" K ! (paye ton café stp le gooooooaaaaaat!!)") )
