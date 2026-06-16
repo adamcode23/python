1e55645101a67fe980d902b84be67846d31e6e30
adamcode23 <adamrz2011@gmail.com>
2026-06-16 13:06:39 +0000

    on a rajouter du contenu

--- pour la météo régale toi BG.py
+++ pour la météo régale toi BG.py
@@ -1,4 +1,8 @@
 celciucs = float(input("quel est la température en celciucs ? "))
+if celciucs > 25:
+    print("waa i fait super chau")
+if celciucs < 15:
+    print("waa i fait super froid")
 farenheit = celciucs * 9 / 5 + 32
 kelvin = celciucs + 273.15
 print(("la température en farenheit est de ") + str(farenheit) + (" °F !") + (" et en kelvin de ") + str(kelvin) + (" K ! (paye ton café stp le gooooooaaaaaat!!)"))
