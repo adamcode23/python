d57a685126e8d1c9a516e96e5e9acd43b0c4b617
adamcode23 <adamrz2011@gmail.com>
2026-06-16 18:32:57 +0200

    amelioration

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ if mot_de_passe == "1234":
     print("mot de passe correct !")
 else: 
     print("mot de passe incorrect !")
-print("Bienvenue dans le programme météo !")
+print("Le programme est terminé !")
