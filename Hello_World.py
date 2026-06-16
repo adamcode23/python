92650d79d09508609e1acb32a38e375cd72512ff
adamcode23 <adamrz2011@gmail.com>
2026-06-16 08:12:18 +0000

    cc les ptit loup

--- Hello_World.py
+++ Hello_World.py
@@ -1,2 +1,2 @@
 age = int(input("ta quel age bg, abonne toi stp je suis en dépréssion dépresive ;( "))
-print(age+1)
+print(("tu as ") + str(age) + (" ans, tu es donc né en ") + str(2026 - age) + (" !"))
