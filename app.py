25609014bf317233c95818f5a85f84f2eb5f5a6a
adamcode23 <adamrz2011@gmail.com>
2026-06-16 18:40:16 +0200

    contenu

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 mot_de_passe = input("quel est le mot de passe ? ")
 
+if len(mot_de_passe) < 4:
+    print("le mot de passe est trop court !")
+
 if mot_de_passe == "1234":
     print("mot de passe correct !")
 else: 
