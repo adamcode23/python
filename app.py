9f8134835d0e3b6b23b781798ea038c0ea11d988
adamcode23 <adamrz2011@gmail.com>
2026-06-16 19:01:00 +0200

    script to check password length and validity

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 mot_de_passe = input("quel est le mot de passe ? ")
 
 if len(mot_de_passe) < 4:
-    print("le mot de passe est trop court !")
-if len(mot_de_passe) > 4:
-    print("le mot de passe est trop long !")
-if len(mot_de_passe) == 0:
-    print("le mot de passe est vide !")
-    print("relancer le programme !")
-
-if mot_de_passe == "1234":
-    print("mot de passe correct !")
-else: 
-    print("mot de passe incorrect !")
+    if len(mot_de_passe) == 0:
+        print("lécrire autre chose")
+    else:
+        print("mot de passe trop court !")
 print("Le programme est terminé !")
