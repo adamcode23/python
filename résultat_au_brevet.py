b4f927b033b29fcac3eef3724f2c83678b46d004
adamcode23 <adamrz2011@gmail.com>
2026-06-17 19:44:20 +0000

    resultat au brevet ameliorere et petite otomatisation (avous de le decouvrir)

--- résultat_au_brevet.py
+++ résultat_au_brevet.py
@@ -1,14 +1,22 @@
-note = int(input("quel est ta note au brevet ? "))
-
+while True:
+    try:
+        note = float(input("quel est ta note au brevet ? "))
+    except ValueError:
+        print(" Entre un nombre valide (utilise un point pour les décimales)😡😡.")
+        continue
+    if note > 20 or note < 0:
+        print("la note doit être comprise entre 0 et 20😡😡")
+        continue
+    break
 if note >= 18:
-    print("tu as réussi ton brevet !, avec mention très bien et félicitation du jury😂")
+    print("tu as réussi ton brevet !, avec mention très bien et félicitation du jury😂😂")
 elif note >= 16:
-    print("tu as réussi ton brevet !, avec mention très bien")
+    print("tu as réussi ton brevet !, avec mention très bien😃😃")
 elif note >= 14:
-    print("tu as réussi ton brevet !, avec mention bien")
+    print("tu as réussi ton brevet !, avec mention bien😉😉")
 elif note >= 12:
-    print("tu as réussi ton brevet !, avec mention assez bien")
+    print("tu as réussi ton brevet !, avec mention assez bien🙂🙂")
 elif note >= 10:
     print("tu as réussi ton brevet !, sans mention")
 else:
-    print("tu as échoué ton brevet !")      
+    print("tu as échoué ton brevet😱😱 !")
