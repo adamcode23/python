4fb16ad453658feca96ca476070cff5fedaf1453
adamcode23 <adamrz2011@gmail.com>
2026-06-17 19:17:41 +0200

    amelioration

--- résultat_au_brevet.py
+++ résultat_au_brevet.py
@@ -1,1 +1,14 @@
-print(input("quel est t'as note au brevet ? "))
+note = int(input("quel est ta note au brevet ? "))
+
+if note >= 18:
+    print("tu as réussi ton brevet !, avec mention très bien et félicitation du jury")
+elif note >= 16:
+    print("tu as réussi ton brevet !, avec mention très bien")
+elif note >= 14:
+    print("tu as réussi ton brevet !, avec mention bien")
+elif note >= 12:
+    print("tu as réussi ton brevet !, avec mention assez bien")
+elif note >= 10:
+    print("tu as réussi ton brevet !, sans mention")
+else:
+    print("tu as échoué ton brevet !")
